4d59c7301b6dc0ae47217f7dc4a85fb6424e957a
Jan Eike Suchard <jan@suchard.cloud>
2025-03-13 09:13:54 +0100

    chore(docs): documenting Configure on router

--- router/router.go
+++ router/router.go
@@ -7,13 +7,19 @@ import (
 	v1Routes "microservice/routes/v1"
 )
 
+// Configure generates a new router and adds routes to the router
+//
+// The router can also be imported during tests, as long as the tests are in a
+// separate package.
+// If the tests are in the same package (e.g. routes defined in `v3` and tests
+// also defined in `v3`) an import cycle exists.
 func Configure() (*gin.Engine, error) {
 	r, err := internal.GenerateRouter()
 	if err != nil {
 		return nil, err
 	}
 
-	/* Define and import your routes here */
+	// TODO: Add your routes in this group
 	v1 := r.Group("/v1")
 	{
 		v1.GET("/", v1Routes.BasicHandler)
